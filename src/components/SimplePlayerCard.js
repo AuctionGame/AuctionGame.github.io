7c235a36b10e391a0613e8e09f54b832d9cb21bd
Priyam <56730716+Signior-X@users.noreply.github.com>
2020-08-25 15:28:44 +0530

    Added players page with  simple card and left with todo auction team storing (#12)
    * Added simple player card and all
    
    * Added players page and left with todo auction team storing

---
+++ src/components/SimplePlayerCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+import players from '../data/players.json';
+
+const useStyles = makeStyles({
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function SimplePlayerCard(props) {
+
+
+  const classes = useStyles();
+  
+  const imgSrc = "cpng/" + props.value + ".jpg";
+
+  return (
+    <div className="col">
+    <Card className="simple-player-card">
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          <img className="gallery-item" src={imgSrc} alt="Player" ></img>
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {players[props.value]}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
+  );
+}
--- src/components/home.js
+++ src/components/home.js
@@ -2,10 +2,59 @@ import React from 'react';
 // import * as firebase from 'firebase';
 import Predict from './predict';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import SimplePlayerCard from './SimplePlayerCard';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+function MiddleBlock(props) {
+  if(props.currentMain) {
+    // Team 1 to 8
+
+    var current = props.teams[props.currentMain];
+
+    var cardsElementList = [];
+    current.map((element, index) => {
+      console.log(element);
+      cardsElementList.push(
+        <SimplePlayerCard value={element} />
+      )
+      return false;
+    })
+    
+
+    return (
+      <div className="row">
+        {cardsElementList}
+      </div>
+    )
+  } else {
+    // Above Auction One
+    return (
+      `Current Auction going for ${props.round} `
+    )
+  }
+}
+
 
 function LeftTabs(props) {
+
+  var heading="Auction Status";
+
+  if(props.value) {
+    heading = "Team " + props.value;
+  }
+
   return (
-    <h1>Auction </h1>
+    <ListItem button
+      key={props.value}
+      onClick={props.handler}
+      >
+      <ListItemText primary={heading} />
+    </ListItem>
   )
 }
 
@@ -14,18 +63,39 @@ class Home extends React.Component {
     super(props);
     this.state = {
       teams: {
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        7: [],
-        8: []
-      }
+        1: [12,14],
+        2: [15,16,19],
+        3: [30,10],
+        4: [1],
+        5: [2],
+        6: [4],
+        7: [5],
+        8: [6,7,9]
+      },
+      currentMain: 0
     }
   }
 
+  updateCurrrentMain(i) {
+    console.log("update current main", i);
+    this.setState({
+      currentMain: i
+    })
+  }
+
+
+  componentDidMount() {
+    const db = firebase.firestore();
+
+    db.collection('team')
+    .onSnapshot(function(snap) {
+      
+      snap.forEach(function(doc) {
+        console.log("TEam data", doc.data());
+      });
+       
+    })
+  }
 
   render() {
     const round = this.props.round;
@@ -33,14 +103,34 @@ class Home extends React.Component {
     if (round === 'prediction') {
       return <Predict />;
     } else {
+
+      // First creation of Left menu
+      const leftBar = [];
+      for(let i=0; i<=8; i++) {
+        // For first one is the auction one
+        leftBar.push(
+          <LeftTabs 
+            key={i}
+            value={i}
+            handler={() => this.updateCurrrentMain(i)}
+          />
+        )
+      }
+
+      const currentMain = this.state.currentMain;
+
       return ( 
         <div id="auction-container">
-          <div class="row">
+          <div className="row">
             <div className="col-sm-3">
-            
+              <List id="left-bar">{leftBar}</List>
             </div>
-            <div class="col-sm-6">
-            Hello Main 
+            <div className="col-sm-6">
+              <MiddleBlock 
+                currentMain={currentMain}
+                teams={this.state.teams}
+                round={this.props.round}
+              />
             </div>
           </div>
         </div> 
