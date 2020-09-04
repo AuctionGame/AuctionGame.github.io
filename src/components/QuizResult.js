0030f88ebad9b220eb94995d501b9a0bb33c9843
parshvajain25 <parshvajain25@gmail.com>
2020-09-04 18:08:21 +0530

    scorecard on client page started

--- src/components/QuizResult.js
+++ src/components/QuizResult.js
@@ -1,3 +1,67 @@
-export default function QuizResult() {
-    return 'quiz result';
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+export default class QuizResult extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            scorecard: {
+                1: 40,
+                2: 35,
+                3: 10
+            }
+        }
+    }
+
+    componentDidMount() {
+        const db = firebase.firestore()
+
+        db.collection("scorecard").onSnapshot(
+            (snap) => {
+                let tempDict = {}
+                snap.forEach(
+                    (doc) => {
+                        tempDict[doc.id] = doc.data().total
+                    }
+                )
+                this.setState ({
+                    scorecard: tempDict
+                })
+            }
+        )
+
+    }
+
+
+
+
+    render() {
+        const scorecard = this.state.scorecard
+        const tableToCreate = []
+        for (var key in scorecard) {
+            tableToCreate.push(
+                <tr>
+                    <td>Team {key}</td>
+                    <td>{scorecard[key]}</td>
+                </tr>
+            )
+        }
+
+        return (
+            <div className="table">
+                <table>
+                    <tr>
+                        <th>Team Name</th>
+                        <th>Team Score</th>
+                    </tr>
+                    <tbody>
+                        {tableToCreate}
+                    </tbody>
+                </table>
+            </div>
+        )
+
+    }
 }
