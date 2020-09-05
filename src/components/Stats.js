332a10b9cc439488d6e3f90ede2fd21045a16124
parshvajain25 <parshvajain25@gmail.com>
2020-09-06 03:34:25 +0530

    stats card completed

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -5,10 +5,32 @@ export default function Stats(props) {
   //Props receive playerno props.playerNo
   var pdata = playerdata[props.playerNo]
   const heading = []
-  for(var key in pdata){
-    heading.push(<td style={{ fontWeight: "bold" }}>{key}</td>)
-  }
+  const data = {}
+  for (const [year, value] of Object.entries(pdata)) {
+    // use key and value
+    heading.push(<td style={{ fontWeight: "bold" }}>{year}</td>)
+    for(const [field,value1] of Object.entries(pdata[year])) {
+      if (data[field]){
+        data[field].push(pdata[year][field])
+      }
+      else{
+        data[field] = [pdata[year][field]]
+      }
 
+    }
+  }
+  const Body = []
+  for(var field in data) {
+    const tempList = []
+    tempList.push(<td style={{ fontWeight: "bold" }}>{field}</td>)
+    for(var ob in data[field]) {
+      tempList.push(<td>{data[field][ob]}</td>)
+    }
+    Body.push(<tr>{tempList}</tr>)
+  }
+  
+  console.log('playerdata: ', data)
+  
   return (
     <div id="player-stats" className="jumbotron" style={{ margin: "22px", padding: "10px" }}>
       <table className="table">
@@ -20,43 +42,9 @@ export default function Stats(props) {
             
           </tr>
         </thead>
+
         <tbody>
-          <tr>
-            <td style={{ fontWeight: "bold" }}>Matches</td>
-            <td>15</td>
-            <td>16</td>
-            <td>16</td>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: "bold" }}>Runs</td>
-            <td>416</td>
-            <td>455</td>
-            <td>290</td>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: "bold" }}>Average</td>
-            <td>83.20</td>
-            <td>75.83</td>
-            <td>26.63</td>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: "bold" }}>Strike Rate</td>
-            <td>135.38</td>
-            <td>150.66</td>
-            <td>116.00</td>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: "bold" }}>Catches</td>
-            <td>11</td>
-            <td>11</td>
-            <td>10</td>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: "bold" }}>Stumpins</td>
-            <td>5</td>
-            <td>3</td>
-            <td>3</td>
-          </tr>
+          {Body}
         </tbody>
       </table>
     </div>
