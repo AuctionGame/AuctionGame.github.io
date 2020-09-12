d4bb2ab483ab5461475f5f1185bca7b0453cbd37
Signior-X <b19188@students.iitmandi.ac.in>
2020-09-13 04:49:47 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5139727f7d5924839ee8c872715064f.js"
+  "/precache-manifest.32c2c695d5e74c03db0630581f509603.js"
 );
 
 self.addEventListener('message', (event) => {
