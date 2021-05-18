2863fb2cb979832690966c16c99fc70011530863
Priyam Seth <b19188@students.iitmandi.ac.in>
2021-05-18 19:38:10 +0530

    Removing all urls starting from slash

--- precache-manifest.32c2c695d5e74c03db0630581f509603.js
+++ precache-manifest.32c2c695d5e74c03db0630581f509603.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ad6ac371edfbee1e12887b09d99c1c54",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "cd55ded45909942cd7df",
-    "url": "/static/css/main.4b06e0b2.chunk.css"
+    "url": "static/css/main.4b06e0b2.chunk.css"
   },
   {
     "revision": "78ebe46c3df770b464c4",
-    "url": "/static/js/2.f055e8ba.chunk.js"
+    "url": "static/js/2.f055e8ba.chunk.js"
   },
   {
     "revision": "8898768082caf1200086aa71f8ab2b10",
-    "url": "/static/js/2.f055e8ba.chunk.js.LICENSE.txt"
+    "url": "static/js/2.f055e8ba.chunk.js.LICENSE.txt"
   },
   {
     "revision": "cd55ded45909942cd7df",
-    "url": "/static/js/main.102a07d5.chunk.js"
+    "url": "static/js/main.102a07d5.chunk.js"
   },
   {
     "revision": "c87281fbbb397148e5d2",
-    "url": "/static/js/runtime-main.678673c0.js"
+    "url": "static/js/runtime-main.678673c0.js"
   }
 ]);
